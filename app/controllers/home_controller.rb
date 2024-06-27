5e92f4af0fd5a576f4696d209966364726515d89
Briton Otieno <ojwangbriton@gmail.com>
2024-06-27 17:56:39 +0300

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    
+    @carousel_images = ["images1.jpeg", "images2.jpeg", "images3.jpeg"]
+    @main_products = Product.take(8)
+    @main_categories = Category.take(4)
   end
 end
